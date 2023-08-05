e1a75ce8958811a79accaf6c97c93d96e798ebb2
jettblu <jettblu.fabrications@gmail.com>
2023-08-05 02:18:48 -0700

    Update number input

--- frontend/components/InputNumber.tsx
+++ frontend/components/InputNumber.tsx
@@ -4,35 +4,42 @@ import { useState } from "react";
 
 export default function InputNumber(props: {
   label: string;
-  doneHandler: (val: number) => any;
+  defaultValue: number;
+  changeHandler: (val: number) => any;
 }) {
-  const { doneHandler, label } = props;
-  const [num, setNum] = useState(".05");
+  const { changeHandler, label, defaultValue } = props;
+  const [num, setNum] = useState("");
 
-  function handleDone() {
-    const newNum = parseInt(num);
+  function handleChange(newVal: string) {
+    const newNum = parseInt(newVal);
+    if (newVal == "") {
+      setNum("");
+      changeHandler(defaultValue);
+      return;
+    }
+    if (newVal == "." || newVal == "0.") {
+      setNum("0.");
+      changeHandler(0);
+      return;
+    }
     if (isNaN(newNum)) {
       console.warn("Not a number");
       return;
     }
-    doneHandler(newNum);
+    setNum(newVal);
+    changeHandler(newNum);
   }
   return (
     <div className="flex flex-col space-y-2">
       <label className="text-sm">{label}</label>
       <div className="flex flex-row w-full rounded-md">
         <input
-          className="rounded-tl-md rounded-bl-md  text-center w-3/4 py-1 bg-gray-50/20 text-2xl focus:outline-none"
+          className="rounded-md text-center w-3/4 py-1 bg-gray-500/20 text-2xl focus:outline-none"
           type="text"
           value={num}
-          onChange={(e) => setNum(e.target.value)}
+          placeholder={defaultValue.toString()}
+          onChange={(e) => handleChange(e.target.value)}
         />
-        <div
-          className="bg-purple-500/80 text-white text-center pt-2 px-3 rounded-tr-md rounded-br-md hover:cursor-pointer hover:bg-purple-500"
-          onClick={handleDone}
-        >
-          Done
-        </div>
       </div>
     </div>
   );
