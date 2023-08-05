baf4a51fe23c678ecfda5f7024f29d7729985e5e
jettblu <jettblu.fabrications@gmail.com>
2023-08-05 02:31:34 -0700

    Add try catch to wasm loader

--- frontend/context/WASM.tsx
+++ frontend/context/WASM.tsx
@@ -13,16 +13,21 @@ export const WASMContextProvider: React.FC<WASMContextProviderProps> = ({
   children,
 }) => {
   async function loadWASMInternal() {
-    if (state.nanograd) {
-      console.log("WASM already loaded");
+    try {
+      if (state.nanograd) {
+        console.log("WASM already loaded");
+        return false;
+      }
+      console.log("Loading WASM...");
+      const nanograd = await import("nanograd_web");
+      await nanograd.default();
+      console.log("WASM loaded");
+      setState({ nanograd, loadWASM: loadWASMInternal });
+      return true;
+    } catch (e) {
+      console.warn("Failed to load WASM \n", e);
       return false;
     }
-    console.log("Loading WASM...");
-    const nanograd = await import("nanograd_web");
-    await nanograd.default();
-    console.log("WASM loaded");
-    setState({ nanograd, loadWASM: loadWASMInternal });
-    return true;
   }
   const [state, setState] = useState<IWASMContext>({
     ...initial,
