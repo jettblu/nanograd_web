bb3e58c593660ab30db0efcd031b386f92dce945
jettblu <jettblu.fabrications@gmail.com>
2023-08-05 02:17:58 -0700

    Update spacing

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 
 import Image from "next/image";
-import GreetButton from "@/components/GreetButton";
 import { WASMContextProvider } from "@/context/WASM";
-import SampleButton from "@/components/SampleButton";
 import { GradientDescent } from "@/components/GradientDescent";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col justify-between">
+      <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex p-24">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Nanograd
+          Minimal deep learning library
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -20,24 +18,17 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            Nanograd
           </a>
         </div>
       </div>
-
       <WASMContextProvider>
-        <GradientDescent />
+        <div className="mx-auto max-w-7xl w-full flex flex-row mb-auto">
+          <GradientDescent />
+        </div>
       </WASMContextProvider>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mx-auto grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
