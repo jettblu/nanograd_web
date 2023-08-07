2325cba552985cccf593b2e875b10332e7f3b36b
jettblu <jettblu.fabrications@gmail.com>
2023-08-07 16:06:30 -0700

    Add data request to rust lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+use serde::Deserialize;
 use wasm_bindgen::prelude::*;
 use nanograd::Value;
 use nanograd::MLP;
+use serde::Serialize;
+use serde_json::from_str;
 
 #[wasm_bindgen]
 extern "C" {
@@ -56,25 +59,31 @@ impl TrainingResult {
     }
 }
 
+#[derive(Deserialize)]
+struct Observation {
+    features: Vec<f64>,
+    label: f64,
+}
+
 #[wasm_bindgen]
-pub fn run_gradient_sample(learning_rate: f64, num_epochs: u8, hidden_layer_sizes:Vec<usize>, fn_callback: js_sys::Function) -> TrainingResult{
-    let input_count = 3;
+pub fn run_gradient_sample(dataset:String, learning_rate: f64, num_epochs: u8, hidden_layer_sizes:Vec<usize>, fn_callback: js_sys::Function) -> TrainingResult{
+    // assuming 2 input features
+    let input_count = 2;
     let mut new_output_dims = hidden_layer_sizes.clone();
     // have final output layer be 1
     new_output_dims.push(1);
     let mlp = MLP::new(input_count, new_output_dims );
-    // our training data
-    let xs = vec![
-        vec![2.0, 3.0, -1.0],
-        vec![3.0, -1.0, 0.5],
-        vec![0.5, 1.0, 1.0],
-        vec![1.0, 1.0, -1.0]
-    ];
+    // load data set from json string
+    let data: Vec<Observation> = from_str(&dataset).unwrap();
+    // our input features
+    let xs = data.iter().map(|o| o.features.clone()).collect::<Vec<Vec<f64>>>();
+    // our ground truth
+    let ys = data.iter().map(|o| o.label).collect::<Vec<f64>>();
     //combine input count and hidden layer sizes for network dimensions
     let mut layer_sizes = hidden_layer_sizes.clone();
     layer_sizes.insert(0, input_count);
-    // our ground truth
-    let ys = vec![1.0, -1.0, -1.0, 1.0];
+    // insert one for output layer... assuming classification task
+    layer_sizes.push(1);
     let mut training_result = TrainingResult::new(vec![], layer_sizes, vec![]);
     // Training loop
     // We train the network for 100 iterations
@@ -128,3 +137,6 @@ pub fn run_gradient_sample(learning_rate: f64, num_epochs: u8, hidden_layer_size
     }
     training_result
 }
+
+
+
