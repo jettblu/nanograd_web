b1dc6f09f227771c81dc7711ae57d2a90ee2803d
jettblu <jettblu.fabrications@gmail.com>
2023-08-08 01:13:41 -0700

    Add worker type descriptions

--- frontend/workers/types.ts
+++ frontend/workers/types.ts
@@ -1,8 +1,17 @@
 import { DatasetName } from "@/data/types";
+import { ITrainingResult, IUpdateResult } from "@/network/types";
 
+/**
+ * The response object sent from the worker.
+ * @property trainingResult The training result.
+ * @property dataFromUpdate The data from the update.
+ * @property timeToTrain The time it took to train.
+ * @property type The type of response.
+ * @property message An optional message.
+ */
 export type NNResponse = {
   trainingResult?: ITrainingResult;
-  dataFromUpdate?: number;
+  dataFromUpdate?: IUpdateResult;
   timeToTrain?: number;
   type: ResponseType;
   message?: string;
@@ -14,15 +23,18 @@ export enum ResponseType {
   Update = "Update",
 }
 
+/**
+ * The request object sent to the worker.
+ * @property learningRate The learning rate hyperparameter.
+ * @property numberOfEpochs The number of epochs to train for.
+ * @property hiddenLayerDims The dimensions of the hidden layers.
+ * @property datasetName The name of the dataset to train on.
+ * @property trainPercent The percentage of the dataset to train on. The rest is used for testing.
+ */
 export type NNRequest = {
   learningRate: number;
   numberOfEpochs: number;
   hiddenLayerDims: Uint32Array;
   datasetName: DatasetName;
-};
-
-export type ITrainingResult = {
-  readonly get_loss: Float64Array;
-  readonly get_network_dimensions: Uint32Array;
-  readonly get_num_epochs: number;
+  trainPercent: number;
 };
