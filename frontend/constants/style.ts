94104b3c4982553d7cbc46e161c53694fbb212b6
jettblu <jettblu.fabrications@gmail.com>
2023-08-08 01:12:19 -0700

    Add style constants

---
+++ frontend/constants/style.ts
@@ -0,0 +1,5 @@
+export const DEFAULT_POINT_COLOR = "rgba(103, 8, 123, 1)";
+export const PINK = "#f52ce4";
+export const BLUE = "#2c7df5";
+export const GREEN = "#2cf5b5";
+export const RED = "#f52c2c";
