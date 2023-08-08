0ce83b9d83906d3ae06b2ebf3899c8278c9d74e4
jettblu <jettblu.fabrications@gmail.com>
2023-08-08 14:55:33 -0700

    Update card spacing

--- frontend/components/CardEpoch.tsx
+++ frontend/components/CardEpoch.tsx
@@ -3,7 +3,7 @@ import { LearningEpoch } from "./GradientDescent";
 export default function CardEpoch(params: { epoch: LearningEpoch }) {
   const { epoch } = params;
   return (
-    <div className="flex flex-row space-x-2 ring-1 ring-purple-400/10 px-1 py-1 brightness-50 hover:brightness-100">
+    <div className="flex flex-row space-x-2 ring-1 ring-purple-400/10 px-1 py-1 brightness-50 hover:brightness-100 w-full">
       <div className="bg-green-900/10 text-white text-center py-1 px-2 rounded-md">
         {epoch.epoch}
       </div>
