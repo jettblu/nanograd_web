0da5318aa547cf0846f513133accc578ec2e2b4c
jettblu <jettblu.fabrications@gmail.com>
2023-08-08 01:12:54 -0700

    Add data return type

--- frontend/data/index.ts
+++ frontend/data/index.ts
@@ -1,10 +1,10 @@
-import { DatasetName } from "./types";
+import { DatasetName, IObservation } from "./types";
 import circleData from "../data/saved/circle.json";
 import spiralData from "../data/saved/spiral.json";
 import xorData from "../data/saved/xor.json";
 import gaussianData from "../data/saved/gaussian.json";
 
-export function loadData(name: DatasetName) {
+export function loadData(name: DatasetName): IObservation[] {
   switch (name) {
     case DatasetName.Circle:
       return circleData;
