d547547cc22fde8a7547926d74e9818ba200214a
jettblu <jettblu.fabrications@gmail.com>
2023-08-08 01:12:10 -0700

    Add network defaults

--- frontend/constants/network.ts
+++ frontend/constants/network.ts
@@ -1,3 +1,7 @@
 export const DEFAULT_HIDDEN_LAYER_SIZES = [2, 3];
 export const DEFAULT_LEARNING_RATE = 0.05;
-export const DEFAULT_NUMBER_OF_EPOCHS = 100;
+export const DEFAULT_NUMBER_OF_EPOCHS = 10;
+export const MAX_TRAIN_PERCENT = 0.8;
+export const MIN_TRAIN_PERCENT = 0.1;
+export const STEP_TRAIN_PERCENT = 0.1;
+export const NUM_OBSERVATIONS = 100;
