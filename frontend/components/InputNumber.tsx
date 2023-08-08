da3ba9edbb754ccb26ab7ff8fa9115e1afc4d22f
jettblu <jettblu.fabrications@gmail.com>
2023-08-08 14:56:42 -0700

    Add error msg to numerical input

--- frontend/components/InputNumber.tsx
+++ frontend/components/InputNumber.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function InputNumber(props: {
   label: string;
   defaultValue: number;
   changeHandler: (val: number) => any;
+  max?: number;
+  min?: number;
 }) {
-  const { changeHandler, label, defaultValue } = props;
+  const { changeHandler, label, defaultValue, min, max } = props;
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
   const [num, setNum] = useState("");
 
   function handleChange(newVal: string) {
@@ -26,9 +29,30 @@ export default function InputNumber(props: {
       console.warn("Not a number");
       return;
     }
+    if (min && newNum < min) {
+      console.warn(`Number must be greater than ${min}`);
+      setErrorMsg(`Number must be greater than ${min}`);
+      return;
+    }
+    if (max && newNum > max) {
+      console.warn(`Number must be less than ${max}`);
+      setErrorMsg(`Number must be less than ${max}`);
+      return;
+    }
     setNum(newVal);
     changeHandler(newNum);
+    setErrorMsg(null);
   }
+  // only show error message for two seconds
+  useEffect(() => {
+    if (errorMsg) {
+      const timeout = setTimeout(() => {
+        setErrorMsg(null);
+      }, 2000);
+      return () => clearTimeout(timeout);
+    }
+  }, [errorMsg]);
+
   return (
     <div className="flex flex-col space-y-2">
       <label className="text-sm">{label}</label>
@@ -38,9 +62,10 @@ export default function InputNumber(props: {
           type="text"
           value={num}
           placeholder={defaultValue.toString()}
-          onChange={(e) => handleChange(e.target.value)}
+          onChange={(e) => handleChange(e.currentTarget.value)}
         />
       </div>
+      {errorMsg && <p className="text-xs text-red-500">{errorMsg}</p>}
     </div>
   );
 }
