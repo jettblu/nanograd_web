20818045353412077bf4e8e0165e97597536c584
jettblu <jettblu.fabrications@gmail.com>
2023-08-09 01:39:27 -0700

    Add about page

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,48 @@
+export default function About() {
+  return (
+    <div className="w-full mx-auto">
+      <div className="">
+        <div className="max-w-3xl mx-auto">
+          <h1 className="text-4xl font-semibold text-left">Nanograd</h1>
+          <p className="text-left text-purple-500 text-xl">
+            A minimal deep learning library.
+          </p>
+        </div>
+      </div>
+
+      <div className="mt-4 prose-xl dark:text-gray-300 max-w-3xl mx-auto">
+        <p>
+          Modern deep learning frameworks are too big. PyTorch and TensorFlow
+          are hundreds of thousands of lines of code; their immense complexity
+          makes adding new features like hardware acceleration difficult.
+        </p>
+        <p>
+          Nanograd is simple. Less code. Fewer operations. Built with Rust 🦀.
+        </p>
+        <h3>Features</h3>
+        <p>
+          Nanograd represents networks as a dynamic DAG. Optimization is handled
+          by backpropagation. However, the DAG only operates over scalar values.
+          This means every neuron is separated into many add and multiply
+          operations: simple, but slow.
+        </p>
+        <p>The next version of nanograd will swap scalars with tensors.</p>
+        <h3>How the Playground Works</h3>
+        <p>
+          The playground allows people to experiment with neural networks in the
+          browser. Change hyperparameters, select a dataset, and add layers: all
+          from a slick interface.
+        </p>
+        <p>
+          Training requests are fulfilled by nanograd. Since nanograd is written
+          in Rust, the code is first compiled into a WebAssembly Module (WASM).
+        </p>
+        <p>
+          Model preferences are shared with a web worker, and the nanograd WASM
+          executes on a separate thread. Results are sent back to the primary
+          thread and displayed.
+        </p>
+      </div>
+    </div>
+  );
+}
