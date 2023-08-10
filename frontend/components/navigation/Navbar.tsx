1644fbb9d8841d39367d83df051349003a8fce17
jettblu <jettblu.fabrications@gmail.com>
2023-08-09 18:04:06 -0700

    Delete bottom nav on small screens

--- frontend/components/navigation/Navbar.tsx
+++ frontend/components/navigation/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
           Deep Learning Playground
         </p>
       </Link>
-      <div className="fixed bottom-0 left-0 flex flex-row h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+      <div className="invisible md:visible fixed bottom-0 left-0 flex flex-row h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
         <Link
           className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
           href="/about"
