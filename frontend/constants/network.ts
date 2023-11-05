d782f74f1ec6707b0031900015985bb03ac7c33e
jettblu <jettblu.fabrications@gmail.com>
2023-11-05 16:02:53 -0500

    Update learning rate defaults

--- frontend/constants/network.ts
+++ frontend/constants/network.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_HIDDEN_LAYER_SIZES = [2];
-export const DEFAULT_LEARNING_RATE = 0.05;
+export const DEFAULT_LEARNING_RATE = 0.01;
 export const DEFAULT_NUMBER_OF_EPOCHS = 10;
 export const MAX_TRAIN_PERCENT = 0.8;
 export const MIN_TRAIN_PERCENT = 0.1;
@@ -8,4 +8,4 @@ export const NUM_OBSERVATIONS = 100;
 export const MAX_EPOCHS = 100;
 export const MIN_EPOCHS = 1;
 export const MAX_LEARNING_RATE = 10;
-export const MIN_LEARNING_RATE = 0.01;
+export const MIN_LEARNING_RATE = 0.0001;
